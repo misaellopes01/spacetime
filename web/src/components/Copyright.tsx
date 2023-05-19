ee65800df445e7b4fa950b4835f56dfc893b1fa3
Misael Lopes <mecl.ely@gmail.com>
2023-05-20 00:10:55 +0100

    Frontend GitHub OAuth

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,25 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+      , by
+      <a
+        href="https://github.com/misaellopes01"
+        target="_blank"
+        className="underline transition-colors hover:text-gray-100"
+        rel="noreferrer"
+      >
+        {' '}
+        Misael Lopes
+      </a>
+    </div>
+  )
+}
